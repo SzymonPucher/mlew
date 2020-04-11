0284088c496a45615b6db68089aa47f58565e435
Szymon Pucher <sajmontube+gitlab@gmail.com>
2020-04-11 21:05:55 +0200

    Add argument parser with nbattles arg

--- python3/run.py
+++ python3/run.py
@@ -1,3 +1,4 @@
+import argparse
 from collections import Counter
 import json
 import pandas
@@ -5,9 +6,20 @@ from tqdm import tqdm
 from config import Config
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--nbattles')
+
+    global _args
+    _args = parser.parse_args()
+
+
 def main():
     stats = []
     config = Config().get_config()
+
+    if _args.nbattles:
+        config.games.number_of_games = _args.nbattles
     
     for _ in tqdm(range(int(config.games.number_of_games)), ascii=True, desc='Playing war games'):
         g = config.games.game_type(config)
@@ -22,4 +34,5 @@ def main():
 
 
 if __name__ == '__main__':
+    parse_args()
     main()
